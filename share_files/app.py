805cd608b8a678f0f6aa881df48a855375318665
richartzCoffee <danielrichartz@hotmail.com>
2025-03-24 22:25:46 -0300

    add endpoint to send a file

--- share_files/app.py
+++ share_files/app.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile
+
+from http import HTTPStatus
 
 app = FastAPI()
 
@@ -6,3 +8,13 @@ app = FastAPI()
 @app.get('/')
 def read_root():
     return {'message': 'Olá Mundo!'}
+
+
+@app.post('/send-file', status_code=HTTPStatus.CREATED)
+async def create_upload_file(file: UploadFile):
+
+    with open(f'./{file.filename}','wb') as new_file:
+        content = await file.read()
+        new_file.write(content)
+
+    return {'filename': file.filename}
